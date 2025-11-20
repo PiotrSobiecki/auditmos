bd0cce35c7e4338b7e955e968de992e0ab72961f
PiotrSobiecki <piotr.sobiecki@gameswift.io>
2025-11-20 16:48:23 +0100

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const basePath = process.env.VITE_PUBLIC_BASE_PATH ?? '/'
+
 export default defineConfig({
+  base: basePath,
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
+  },
 })
